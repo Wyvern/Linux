a55a6592f71c4ff8227c0605985eb190f1044552
Larry Liu <Wyvern@users.noreply.github.com>
2019-05-03 09:50:32 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -33,50 +33,15 @@ $(curl -fsSL https://raw.githubusercontent.com/Wyvern/Linux/s390x/.alias)
 EOF
 
 
-wget https://github.com/git/git/archive/v2.21.0.tar.gz
-tar xf v2.21.0.tar.gz 
-cd git-2.21.0 
-
-yum install -y autoconf curl-devel expat-devel openssl-devel perl-devel zlib-devel libffi-devel
-make configure
-./configure --prefix=/usr/local --without-tcltk
-make NO_TCLTK=Yes NO_MSGFMT=Yes NO_GETTEXT=Yes LDFLAGS+=-s -j install 
-yum remove -y git
-yum autoremove -y
-wget https://nodejs.org/dist/latest-v8.x/node-v8.16.0-linux-s390x.tar.gz
-tar xf node-v8.16.0-linux-s390x.tar.gz
-cd node-v8.16.0-linux-s390x
-cp -r bin/ include/ lib/ share/ /usr/local/
-npm -g up
-
-wget https://download.docker.com/linux/static/stable/s390x/docker-18.06.3-ce.tgz
-tar xf docker-18.06.3-ce.tgz
-cd docker
-cp * /usr/local/bin
-dockerd &
-
-docker run --name vpn --restart=always -d -p 500:500/udp -p 4500:4500/udp --privileged -v /lib/modules:/lib/modules:ro  wyvern/strongswan:ssl
-docker run --name ss --restart=always -d -p 443:443/tcp -p 443:443/udp   wyvern/ss:ibm
-docker run --name bt --restart=always -d -p 80:123 -v /bt:/downloads wyvern/ct:ibm  --port 123 -a bt:asdf
-docker run --name kms --restart=always -d -p 1688:1688 wyvern/kms:ibm
+sh -c “$(curl -fsSL https://raw.githubusercontent.com/Wyvern/Linux/s390x/git-node-docker)”
 
 cd ~
 
-git clone https://github.com/Wyvern/Python.git
-cd Python/
-./configure --prefix=/usr/local --enable-optimizations --with-lto
-make LDFLAGS+=-s -j install
+sh -c “$(curl -fsSL https://raw.githubusercontent.com/Wyvern/Linux/s390x/Python”
 
 cd ~
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-pip install pip-review pkgconfig speedtest-cli
-pip install onedrivecmd google-api-python-client oauth2client progressbar2
-wget https://github.com/tokland/youtube-upload/archive/master.zip
-unzip master.zip
-cd youtube-upload-master/
-python3 setup.py install
+sh -c “$(curl -fsSL https://raw.githubusercontent.com/Wyvern/Linux/s390x/pip”
 
 yum autoremove -y
 package-cleanup -y --oldkernels --count=1
