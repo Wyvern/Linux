7a3142be52d41b6acb08d93edceff8d11f4ef008
Larry Liu <Wyvern@users.noreply.github.com>
2019-09-22 12:33:41 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -45,8 +45,6 @@ package-cleanup -y --oldkernels --count=1
 cd ~
 rm -rf *
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 cat<<EOF>.zshrc
 $(curl -fsSL https://raw.githubusercontent.com/Wyvern/Linux/s390x/.zshrc)
 EOF
@@ -54,5 +52,7 @@ EOF
 cat<<EOF>/root/.alias
 $(curl -fsSL https://raw.githubusercontent.com/Wyvern/Linux/s390x/.alias)
 EOF
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/wyvern/odyt
