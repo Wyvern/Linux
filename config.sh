9cc7a32b2c5c60a4b121f36b6efb39c90fa0eacf
Larry Liu <Wyvern@users.noreply.github.com>
2020-01-02 05:30:58 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -54,5 +54,3 @@ $(curl -fsSL https://raw.githubusercontent.com/Wyvern/Linux/s390x/.alias)
 EOF
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-//git clone https://github.com/wyvern/odyt
